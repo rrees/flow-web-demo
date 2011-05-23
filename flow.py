0ac5cddf39c1cacda395143da095e4a8b587ea8e
Robert Rees <spam@shudderfix.com>
2011-05-23 23:36:33 +0100

    Creating a start point for completing the flow

--- flow.py
+++ flow.py
@@ -10,7 +10,12 @@ db = GraphDatabase("http://localhost:7474/db/data/")
 def front_page():
 	flows = [rel.end for rel in db.nodes[0].relationships.outgoing(["Flow"])]
 	return template('index', flows = flows)
+
+@route('/start/:id')
+def start(id):
+	flow_start = [rel.end for rel in db.nodes[0].relationships.outgoing(["Flow"]) if rel.end['id'] == id]
 	
+	return flow_start[0]['description']
 	
 debug(True)
 run(reloader = True)
